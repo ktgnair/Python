10ed6be6cecaaf5b73ad1bc738753dfa194ddc22
ktgnair <ktgnair95@gmail.com>
2021-05-03 22:19:08 +0530

    Functions in Python

---
+++ day_6.py
@@ -0,0 +1,9 @@
+# Functions in Python
+
+# Defining a function
+def my_function():
+    print("Hello")
+    print("This is user defined function. The function name is my_function()")
+
+# Calling a function
+my_function()
