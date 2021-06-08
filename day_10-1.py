c57534f427da79df5099e31c5d3639cd710cb531
ktgnair <ktgnair95@gmail.com>
2021-06-08 21:23:19 +0530

    Days in Month - Program which takes the input as year and month of your choice and it should display the number of days in that selected month

---
+++ day_10-1.py
@@ -0,0 +1,27 @@
+# Days in Month
+# Write a program which takes the input as year and month of your choice and it should display the number of days in that selected month
+
+def is_leap(year):
+    if year % 4 == 0:
+        if year % 100 == 0:
+            if year % 400 == 0:
+                return True
+            else:
+                return False   
+        else:
+            return True 
+    else:
+        return False
+
+def days_in_month(year, month):
+    month_days = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+    if is_leap(year) and month == 2:
+        return 29
+    return month_days[month-1]
+
+year = int(input("Please enter a year of your choice: "))
+month = int(input("Please enter the month: "))
+days = days_in_month(year, month)
+print(days)
+
+# Docstrings 
