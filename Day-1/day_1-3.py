353efaacf5809ff896d00dc175511bd788e599f0
ktgnair <ktgnair95@gmail.com>
2021-06-12 16:40:52 +0530

    Removed unwanted folder

--- Day-1/day_1-3.py
+++
@@ -1,2 +0,0 @@
-# Write a program that prints the number of characters in a user's name.
-print( len( input("What is you Name?" ) ) )
--- Day-1/day_1-6.py
+++
@@ -1,5 +0,0 @@
-# Band Name Generator
-print("Welcome to the Band Name Generator.")
-cityName = input("What's the name of the city you grew up in?\n")
-petName = input("What's your pet's name?\n")
-print("Your Band Name could be " + cityName + " " + petName)
